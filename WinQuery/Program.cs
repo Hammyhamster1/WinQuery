1308dd0c89efa9491ca23df2f3ae6947277169e6
MoominHaus <MoominHaus@DESKTOP-BBQ4EVO>
2025-12-15 17:38:06 -0500

    Add project files.

---
+++ WinQuery/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Globalization;
+using System.Security.Principal;
+
+namespace WinQuery
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+              Console.WriteLine("Welcome to WinQuery!");
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Windows Version:");
+            Console.WriteLine(Environment.OSVersion.Version);
+            //Lists Your Windows installation Version
+            Console.WriteLine("Machine name:");
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine("User Name:");
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine("Cpu Usage");
+            Console.WriteLine(Environment.CpuUsage.TotalTime);
+            
+
+        }
+    }
+}
